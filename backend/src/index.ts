078e0d7f4ec27f450835fc1ded5e417ee4854278
melanie-saunier <melanie.sau@wanadoo.fr>
2025-10-22 15:47:34 +0200

    build: settings middleware cors not found errorHandler + settings router

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,30 @@
-import 'dotenv/config';
+// Importation des modules nécessaires
+import "dotenv/config";
 import express from "express";
+import { router } from "./routers/router";
+import corsParser from "./middlewares/cors-middleware";
+import { notFound, errorHandler } from "./middlewares/errors-middleware";
 
+// Création d'une app Express
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("ok");
-})
+// Appel au middleware de CORS, afin que la politique des CORS soit vérifiée avant de passe rà la suite
+app.use(corsParser);
 
+// Ajout du bordy parser
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Brancher le router
+app.use(router);
+
+// Middlewares de gestion d'erreurs
+app.use(notFound);
+app.use(errorHandler);
+
+// Définition des variables pour connecter le serveur
 const port = process.env.PORT || 3001;
 const base_url = process.env.BASE_URL || "localhost";
 
+// Démarrage du serveur
 app.listen(port, () => console.log(`Listening on http://${base_url}:${port}`));
---
+++ backend/src/middlewares/errors-middleware.ts
@@ -0,0 +1,21 @@
+import { NextFunction, Request, Response } from "express";
+
+// Création d'une interface qui étend la classe Erreur pour ajouter un statut à l'erreur
+interface AppError extends Error {
+  statusCode?: number;
+}
+
+// Fonction pour les erreurs 404
+function notFound(_req: Request, res: Response) {
+    const err = new Error("Ressource not found");
+    res.status(404).json({ error: err.message });
+}
+
+// Fonction pour les erreurs 500
+function errorHandler(err: AppError, _req: Request, res: Response, _next: NextFunction) {
+    console.log(err);
+    const statusCode = err.statusCode || 500;
+    res.status(statusCode).render(statusCode.toString(), { error: err.message });
+}
+
+export { notFound, errorHandler }
