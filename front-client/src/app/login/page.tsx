e34b9b878140a2765e3069d879d0c25b6a48790f
melanie-saunier <melanie.sau@wanadoo.fr>
2025-10-26 11:16:06 +0100

    front/build: integration login page

---
+++ front-client/src/app/login/page.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import Button from "@/components/Button";
+import Link from "next/link";
+import { useEffect, useRef } from "react";
+
+export default function LoginPage() {
+  // pour faire un focus sur le premier input(input email)
+  const inpuRef = useRef<HTMLInputElement>(null);
+  useEffect(() => {
+    inpuRef.current?.focus();
+  }, []);
+  return(
+    <div className="relative bg-[url('/images/background.png')] bg-no-repeat bg-cover bg-center min-h-[600px] md:min-h-screen p-4 md:p-8">
+      {/* div pour un effet overlay (noir) pour que le text ressorte bien par dessus l'image de background */}
+      <div className="absolute inset-0 bg-neutral-700/50"></div>
+      <div className="relative z-10">
+        <h2 className="text-xl md:text-2xl text-center">INSCRIPTION</h2>
+        <form className="flex flex-col items-center gap-4">
+          <div className="flex flex-col">
+            {/* honeypot pour protéger des attaques de robot: le robot va remplir cet input */}
+            {/* on pourra donc rejeter la soumission du form si cet input est complété */}
+            {/* tabIndex -1 pour ne pas accèder à cet index avec la touche tab et autocomplete:off pour pas que le navigateur ne complète automatiquement l'input */}
+            <input type="text" name="honeypot" className="hidden" tabIndex={-1} autoComplete="off"/>
+          </div>
+          <div className="flex flex-col w-64 md:w-80">
+            <label htmlFor="email" className="font-bold">E-mail</label>
+            <input
+              ref={inpuRef} 
+              type="email" 
+              name="email" 
+              id="email" 
+              required 
+              className="input_style"
+            />
+          </div>
+          
+          <div className="flex flex-col w-64 md:w-80">
+            <label htmlFor="password" className="font-bold">Mot de passe</label>
+            <input 
+              type="password" 
+              name="password" 
+              id="password" 
+              className="input_style" 
+              required/>
+          </div>
+          <Button style="button_booking" text="S'INSCRIRE"/>
+        </form>
+        <p>Pas encore de compte ? <Link href={"/register"}>Créez un compte ici</Link></p>
+      </div>
+    </div>
+  );
+}
