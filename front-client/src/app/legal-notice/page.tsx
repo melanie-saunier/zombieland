8195db354b190d24e69db7250c25b613b65cbadf
Kari Routier <0l0.kari.0l0@gmail.com>
2025-10-27 20:41:49 +0100

    finished tailwind css

--- front-client/src/app/legal-notice/page.tsx
+++ front-client/src/app/legal-notice/page.tsx
@@ -4,38 +4,34 @@
 export default function legalNoticePage() {
 
   return (
-    <div className="bg-radial from-[#961990] to-[#000000] p-8">
-      <h1 className="flex items-center text-shadow-none! tracking-normal! justify-center p-6">MENTION LÉGALES</h1>
+    <div className="bg-radial from-[#961990] to-[#000000] p-12">
+      <h1 className="flex items-center text-shadow-none! tracking-normal! justify-center pt-2 pl-8 pr-8 pb-8">MENTION LÉGALES</h1>
       <p className="">Conformément aux dispositions de la loi n° 2004-575 du 21 juin 2004 pour la confiance en l&apos;économie numérique, il est précisé aux utilisateurs du site Zombieland l&apos;identité des différents intervenants dans le cadre de sa réalisation et de son suivi.</p>
 
-      <h2 className="text-shadow-none! tracking-normal! p-6">Edition du site</h2>
+      <h2 className="text-shadow-none! tracking-normal! pl-6 pt-6 pr-6 pb-2">Edition du site</h2>
       <p className="">Le présent site, accessible à l’URL www.zombieland.fr (le « Site »), est édité par :</p>
       <p>Emilie Dupont, résidant 10 Bd de Parc, 77700 Coupvray, de nationalité Française (France), né(e) le 05/05/1998,  inscrite au R.C.S. de VERSAILLES sous le numéro RCS VANNES B 514 919 845,</p>
       <p>Le numéro individuel TVA de l’éditeur est : FR 40 987654321.</p>
 
-      <h2 className="text-shadow-none! tracking-normal! p-6">Hébergement</h2>
+      <h2 className="text-shadow-none! tracking-normal! pl-6 pt-6 pr-6 pb-2">Hébergement</h2>
       <p>Le Site est hébergé par la société O Clock, situé 10 r Penthièvre, 75008 Paris, (contact téléphonique ou email : +33189716530).</p>
 
-      <h2 className="text-shadow-none! tracking-normal! p-6">Directeur de publication</h2>
+      <h2 className="text-shadow-none! tracking-normal! pl-6 pt-6 pr-6 pb-2">Directeur de publication</h2>
       <p>Le Directeur de la publication du Site est Emilie Dupont.</p>
 
-      <h2 className="text-shadow-none! tracking-normal! p-6">Nous contacter</h2>
+      <h2 className="text-shadow-none! tracking-normal! pl-6 pt-6 pr-6 pb-2">Nous contacter</h2>
       <p>Par téléphone : +33189716530
         Par email : contact@Oclock.io
         Par courrier : 10 Bd de Parc, 77700 Coupvray</p>
 
-      <h2 className="text-shadow-none! tracking-normal! p-6">Données personnelles</h2>
+      <h2 className="text-shadow-none! tracking-normal! pl-6 pt-6 pr-6 pb-2">Données personnelles</h2>
       <p>Le traitement de vos données à caractère personnel est régi par notre Charte du respect de la vie privée, disponible depuis la section &quot;Charte de Protection des Données Personnelles&quot;, conformément au Règlement Général sur la Protection des Données 2016/679 du 27 avril 2016 («RGPD»).</p>
       <p>a désigné un Délégué à la Protection des Données (DPO) auprès de la CNIL (Désignation N° ). Les coordonnées de notre Délégué à la Protection des Données sont les suivantes :</p>
 
-      <p>Nom : Arthur Smith
+      <p className="p-3!">Nom : Arthur Smith
         Adresse : 2 rue de l&apos;échappée 75010 PARIS
         Téléphone : +33610358691
         Email : asmith@gmail.com</p>
-
-      <p>Génération des mentions légales par Legalstart.</p>
-
-
     </div>
   );
 }
--- front-client/src/app/privacy-policy/page.tsx
+++ front-client/src/app/privacy-policy/page.tsx
@@ -3,56 +3,56 @@
 export default function privacyPolicyPage() {
 
   return (
-    <div className="bg-radial from-[#961990] to-[#000000] p-8">
+    <div className="bg-radial from-[#961990] to-[#000000] p-12">
       <h1 className="flex items-center justify-center text-shadow-none! tracking-normal! p-6">Politique de Confidentialité – Zombieland</h1>
-      <h2 className="text-shadow-none! tracking-normal! p-6">Dernière mise à jour : Octobre 2025</h2>
+      <h2 className="text-shadow-none! tracking-normal! normal-case! p-6">Dernière mise à jour : Octobre 2025</h2>
       <p>Chez Zombieland, nous prenons très au sérieux la protection de vos données personnelles. Cette politique de confidentialité explique comment nous collectons, utilisons, partageons et protégeons les informations que vous nous confiez lorsque vous utilisez notre site web et nos services.</p>
 
-      <h2 className="text-shadow-none! tracking-normal! p-6">1. Quelles données collectons-nous ?</h2>
+      <h2 className="text-shadow-none! tracking-normal! pl-6 pt-6 pr-6 pb-2">1. Quelles données collectons-nous ?</h2>
       <h3 className="">1. Nous pouvons collecter les informations suivantes :</h3>
       <ul className="list-inside list-disc">
-        <li>Données d’identification : nom, prénom, adresse e-mail, numéro de téléphone, adresse postale (lors de la création d’un compte ou d’une réservation).</li>
-        <li>Données de paiement : informations de carte bancaire (traitées de manière sécurisée par nos partenaires de paiement).</li>
-        <li>Données de navigation : adresse IP, type de navigateur, pages visitées, durée de la visite, etc. (via des cookies et outils d’analyse).</li>
-        <li>Données de réservation : date et heure de visite, type de billet, préférences d’activités.</li>
+        <li><span className="font-semibold">Données d’identification</span> : nom, prénom, adresse e-mail, numéro de téléphone, adresse postale (lors de la création d’un compte ou d’une réservation).</li>
+        <li><span className="font-semibold">Données de paiement</span> : informations de carte bancaire (traitées de manière sécurisée par nos partenaires de paiement).</li>
+        <li><span className="font-semibold">Données de navigation</span> : adresse IP, type de navigateur, pages visitées, durée de la visite, etc. (via des cookies et outils d’analyse).</li>
+        <li><span className="font-semibold">Données de réservation</span> : date et heure de visite, type de billet, préférences d’activités.</li>
       </ul>
 
-      <h2 className="text-shadow-none! tracking-normal! p-6">2. Pourquoi collectons-nous ces données ?</h2>
+      <h2 className="text-shadow-none! tracking-normal! pl-6 pt-6 pr-6 pb-2">2. Pourquoi collectons-nous ces données ?</h2>
       <h3 className="">Vos données sont utilisées pour :</h3>
       <ul className="list-inside list-disc">
-        <li>Gérer vos réservations et vous fournir un accès fluide au parc.</li>
-        <li>Personnaliser votre expérience (offres spéciales, recommandations d’attractions).</li>
-        <li>Améliorer nos services (analyse des comportements de visite, feedbacks).</li>
-        <li>Assurer la sécurité du site et des transactions.</li>
-        <li>Respecter nos obligations légales (facturation, archives).</li>
+        <li><span className="font-semibold">Gérer vos réservations</span> et vous fournir un accès fluide au parc.</li>
+        <li><span className="font-semibold">Personnaliser votre expérience</span> (offres spéciales, recommandations d’attractions).</li>
+        <li><span className="font-semibold">Améliorer nos services</span> (analyse des comportements de visite, feedbacks).</li>
+        <li><span className="font-semibold">Assurer la sécurité</span> du site et des transactions.</li>
+        <li><span className="font-semibold">Respecter nos obligations légales</span> (facturation, archives).</li>
       </ul>
 
-      <h2 className="text-shadow-none! tracking-normal! p-6">3. Comment protégeons-nous vos données ?</h2>
+      <h2 className="text-shadow-none! tracking-normal! pl-6 pt-6 pr-6 pb-2">3. Comment protégeons-nous vos données ?</h2>
       <p>Nous mettons en place des mesures de sécurité techniques et organisationnelles pour protéger vos informations contre tout accès non autorisé, perte ou divulgation. Vos données de paiement sont chiffrées et traitées par des prestataires agréés.</p>
 
-      <h2 className="text-shadow-none! tracking-normal! p-6">4. Partage des données</h2>
+      <h2 className="text-shadow-none! tracking-normal! pl-6 pt-6 pr-6 pb-2">4. Partage des données</h2>
       <h3 className="">Nous ne vendons ni ne louons vos données personnelles à des tiers. Vos informations peuvent être partagées avec :</h3>
       <ul className="list-inside list-disc">
-        <li>Nos prestataires de services (hébergement, paiement, analyse) sous contrat de confidentialité.</li>
-        <li>Les autorités compétentes, si la loi l’exige.</li>
+        <li><span className="font-semibold">Nos prestataires de services</span> (hébergement, paiement, analyse) sous contrat de confidentialité.</li>
+        <li><span className="font-semibold">Les autorités compétentes</span>, si la loi l’exige.</li>
       </ul>
 
 
-      <h2 className="text-shadow-none! tracking-normal! p-6">5. Vos droits</h2>
+      <h2 className="text-shadow-none! tracking-normal! pl-6 pt-6 pr-6 pb-2">5. Vos droits</h2>
       <h3 className="">Conformément au RGPD, vous disposez des droits suivants :</h3>
       <ul className="list-inside list-disc">
-        <li>Accéder à vos données personnelles.</li>
-        <li>Les rectifier ou les supprimer.</li>
-        <li>Vous opposer à leur traitement.</li>
-        <li>Demander leur portabilité.</li >
-        <li>Retirer votre consentement à tout moment.</li>
+        <li><span className="font-semibold">Accéder</span> à vos données personnelles.</li>
+        <li><span className="font-semibold">Les rectifier</span> ou <span className="font-semibold">les supprimer</span>.</li>
+        <li><span className="font-semibold">Vous opposer</span> à leur traitement.</li>
+        <li><span className="font-semibold">Demander leur portabilité.</span></li >
+        <li><span className="font-semibold">Retirer votre consentement</span> à tout moment.</li>
       </ul>
       <p>Pour exercer ces droits, contactez-nous à : contact@zombieland.fr.</p>
 
-      <h2 className="text-shadow-none! tracking-normal! p-6">6. Cookies et technologies similaires</h2>
+      <h2 className="text-shadow-none! tracking-normal! pl-6 pt-6 pr-6 pb-2">6. Cookies et technologies similaires</h2>
       <p>Notre site utilise des cookies pour améliorer votre expérience et analyser le trafic. Vous pouvez gérer vos préférences de cookies via notre bannière de consentement ou les paramètres de votre navigateur.</p>
 
-      <h2 className="p-6 text-shadow-none! tracking-normal!">7. Modifications de cette politique</h2>
+      <h2 className=" text-shadow-none! tracking-normal! pl-6 pt-6 pr-6 pb-2">7. Modifications de cette politique</h2>
       <p>Nous nous réservons le droit de mettre à jour cette politique. Toute modification sera publiée sur cette page avec la date de mise à jour.</p>
 
       <h3 className="">Contact</h3>
