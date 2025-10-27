d05c0add0d71e2f2621c55f4d51ff3bb1a4761c6
manon-thez <m.thez@live.fr>
2025-10-27 16:20:56 +0100

    (back)fix: change the id type in sequelize models (UUID --> number)

--- backend/src/models/role.ts
+++ backend/src/models/role.ts
@@ -2,27 +2,28 @@ import { Model, DataTypes } from "sequelize";
 import { sequelize } from "./sequelize";
 
 export class Role extends Model {
-  declare id: string;
+  declare id: number;
   declare name: string;
   declare readonly created_at: Date;
   declare readonly updated_at: Date;
 }
 
 Role.init(
-    {
-      id: {
-        type: DataTypes.UUID,
-        defaultValue: DataTypes.UUIDV4,
-        primaryKey: true,
-      },
-      name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
     },
-    {
-      sequelize,
-      modelName: "Role",
-      tableName: "role"
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
     },
-  );
+  },
+  {
+    sequelize,
+    modelName: "Role",
+    tableName: "role"
+  },
+);
