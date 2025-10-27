ac600c2daa9bf070f8d908db80ec22ea71e748c4
melanie-saunier <melanie.sau@wanadoo.fr>
2025-10-27 16:38:50 +0100

    front/fetch-activities/chore: add axios and setting backend

---
+++ front-client/src/api/activites.ts
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
+
+// recupérer la liste de toutes les activités de l'API
+export async function fetchAllActivities() {
+  try {
+    const res = await axios.get(`${API_URL}/activities`);
+    return res.data;
+  } catch (error) {
+    console.error("Erreur lors de la récupération des activitées");
+    return;
+  }
+};
