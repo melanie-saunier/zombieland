59e2dd28346a5f98eb6b41af32d56d0d98a5cc32
manon-thez <m.thez@live.fr>
2025-10-27 14:57:43 +0100

    (back)fix: change the type of id in script create tables (UUID --> INT)

--- backend/data/create-table.sql
+++ backend/data/create-table.sql
@@ -3,9 +3,6 @@ BEGIN;
 DROP TABLE IF EXISTS "role", "user", "price", "booking_price", "booking", "category", "level", "activity" CASCADE;
 DROP DOMAIN IF EXISTS "email_str", "hex_color";
 
--- Extension PostgreSQL pour gérer les UUID
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-
 -- Création de domain 
 -- Pour l'email :
 CREATE DOMAIN email_str AS TEXT
@@ -18,7 +15,7 @@ CREATE DOMAIN hex_color AS CHAR(7)
 -- Création des tables
 -- Table: role
 CREATE TABLE role (
-  id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   name VARCHAR(255) NOT NULL,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
@@ -26,19 +23,19 @@ CREATE TABLE role (
 
 -- Table: user
 CREATE TABLE "user" (
-  id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   email email_str NOT NULL UNIQUE,
   lastname VARCHAR(255) NOT NULL,
   firstname VARCHAR(255) NOT NULL,
   password VARCHAR(255) NOT NULL,
-  role_id UUID NOT NULL REFERENCES role(id),
+  role_id INT NOT NULL REFERENCES role(id),
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 -- Table: level
 CREATE TABLE level (
-  id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   name VARCHAR(255) NOT NULL,
   value INTEGER NOT NULL CHECK (value IN (1, 2, 3)),
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
@@ -47,7 +44,7 @@ CREATE TABLE level (
 
 -- Table: category
 CREATE TABLE category (
-  id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   name VARCHAR(255) NOT NULL,
   color hex_color NOT NULL,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
@@ -56,22 +53,22 @@ CREATE TABLE category (
 
 -- Table: activity
 CREATE TABLE activity (
-  id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   name VARCHAR(255) NOT NULL,
   description TEXT NOT NULL,
   duration INTEGER NOT NULL CHECK (duration > 0),
   min_height INTEGER NOT NULL CHECK (min_height >= 0),
   pregnancy_warning BOOLEAN NOT NULL,
   image_ref VARCHAR(255) NOT NULL,
-  level_id UUID NOT NULL REFERENCES level(id),
-  category_id UUID NOT NULL REFERENCES category(id),
+  level_id INT NOT NULL REFERENCES level(id),
+  category_id INT NOT NULL REFERENCES category(id),
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 -- Table: price
 CREATE TABLE price (
-  id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   label VARCHAR(255) NOT NULL,
   value NUMERIC(10,2) NOT NULL CHECK (value > 0),
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
@@ -80,21 +77,21 @@ CREATE TABLE price (
 
 -- Table: booking
 CREATE TABLE booking (
-  id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   visit_date DATE NOT NULL,
   nb_people INTEGER NOT NULL CHECK (nb_people > 0),
   status BOOLEAN NOT NULL,
-  user_id UUID NOT NULL REFERENCES "user"(id),
+  user_id INT NOT NULL REFERENCES "user"(id),
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 -- Table: booking_price
 CREATE TABLE booking_price (
-  id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   applied_price NUMERIC(10,2) NOT NULL CHECK (applied_price > 0),
-  booking_id UUID NOT NULL REFERENCES booking(id),
-  price_id UUID NOT NULL REFERENCES price(id),
+  booking_id INT NOT NULL REFERENCES booking(id),
+  price_id INT NOT NULL REFERENCES price(id),
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
