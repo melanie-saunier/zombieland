ec90834801b305ac8fba4d640f1978ad29f95044
manon-thez <m.thez@live.fr>
2025-10-27 10:52:24 +0100

    (front) fix: add comments on activity detail page and creation of RenderSkulls component

--- front-client/src/app/activity/[id]/page.tsx
+++ front-client/src/app/activity/[id]/page.tsx
@@ -1,8 +1,10 @@
-import { Skull, TriangleAlert } from "lucide-react";
+import renderSkulls from "@/components/RenderSkulls";
+import { TriangleAlert } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 
 type ActivityDetailPageProps = {
+  // TODO: si on modifie le type de id activity (UUID à integer) alors il faudra modifier string en integer
   params: Promise<{ id: string }>;
 };
 
@@ -11,7 +13,7 @@ export default async function ActivityDetailPage({params}: ActivityDetailPagePro
   // on reçoit direct en props du composant une promesse avec les valeurs des segments dynamiques
   const { id } = await params;
 
-  // TODO: fetch à faire quand back sera prête, en attendant, on a objet activity à la place (provisoire, pour mise en place du front)
+  // TODO: fetch à faire quand back sera prêt. En attendant, on a un objet "activity" à la place (provisoire, pour mise en place du front)
   const activity =
     { id: "activity-id1",
       name: "Target Panic",
@@ -23,24 +25,6 @@ export default async function ActivityDetailPage({params}: ActivityDetailPagePro
       category: {id: "category-id2", name: "Instinct de survie", color: "#C41E3A"},
       level: {id: "level-id1", name: "Facile", value: 2} 
     };
-    // Fonction utilitaire pour afficher les skulls selon le niveau
-  const renderSkulls = (levelValue: number) => {
-    const skulls = [];
-    for (let i = 1; i <= 3; i++) {
-      skulls.push(
-        <Skull
-          key={i}
-          size={24}
-          color={
-            i <= levelValue
-              ? "var(--color-secondary-200)" // skull "rempli"
-              : "var(--color-secondary-500)" // skull "vide"
-          }
-        />
-      );
-    }
-    return skulls;
-  };
 
   return (
     <div className="bg-neutral-700 py-4 px-8">
@@ -65,7 +49,9 @@ export default async function ActivityDetailPage({params}: ActivityDetailPagePro
         {/* {Conteneur du texte} */}
         <div className="md:w-1/2 flex flex-col gap-2 md:gap-8 md:px-8">
           <div>
+            {/* {Nom de l'activité} */}
             <h1 className="text-3xl md:text-4xl pb-4">{activity.name}</h1>
+            {/* {Badge de la catégorie} */}
             <span 
               className="rounded-xl text-neutral-50 px-2 md:text-sm text-xs font-medium w-fit"
               style={{ backgroundColor: activity.category.color }}
@@ -75,6 +61,7 @@ export default async function ActivityDetailPage({params}: ActivityDetailPagePro
           </div>
           <p className="text-justify italic">{activity.description}</p>
           <div>
+            {/* {Niveau de frisson avec les crânes en fonction du niveau} */}
             <h2 className="font-bold text-xl pb-1">Niveau de frisson</h2>
             <div className="flex items-center gap-2">
               {renderSkulls(activity.level.value)}
@@ -82,15 +69,18 @@ export default async function ActivityDetailPage({params}: ActivityDetailPagePro
           </div>
           <div className="flex justify-between items-center gap-2">
             <div>
+              {/* {Durée de l'activité} */}
               <h2 className="font-bold text-xl pb-1">Durée</h2>
               <p>{activity.duration} min</p>
             </div>
             <div>
+              {/* {Taille minimum pour participer à l'activité} */}
               <h2 className="font-bold text-xl pb-1">Taille Minimum</h2>
               <p>{activity.min_height} cm</p>
             </div>
           </div>
           {
+            /* Avertissement femme enceinte : si pregnancy_warning = true, on affiche cette div, sinon, on ne l'affiche pas */
             activity.pregnancy_warning &&
             <div className="flex items-center gap-2">
               <TriangleAlert size={24} color="var(--color-primary-purple-300)" className="w-1/4 md:w-auto"/><p className="italic">Cette attraction est fortement déconseillée aux femmes enceintes.</p>
