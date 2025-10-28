3f0d2d11f90de862fce5d73752053d13c48114dc
manon-thez <m.thez@live.fr>
2025-10-28 13:31:03 +0100

    (front)fix: review of cardActivity

--- front-client/src/components/CardActivity.tsx
+++ front-client/src/components/CardActivity.tsx
@@ -1,5 +1,6 @@
-import { Link, Skull } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
+import renderSkulls from "./RenderSkulls";
 
 type PropsCardActivity = {
   activity: {
@@ -16,41 +17,26 @@ type PropsCardActivity = {
 };
 
 export default function CardActivity({activity} : PropsCardActivity) {
-  // Fonction utilitaire pour afficher les skulls selon le niveau
-  const renderSkulls = (levelValue: number) => {
-    const skulls = [];
-    for (let i = 1; i <= 3; i++) {
-      skulls.push(
-        <Skull
-          key={i}
-          size={24}
-          color={
-            i <= levelValue
-              ? "var(--color-secondary-200)" // skull "rempli"
-              : "var(--color-secondary-500)" // skull "vide"
-          }
-        />
-      );
-    }
-    return skulls;
-  };
+
   return (
     <article className="relative flex flex-col border-solid border-2 border-primary-purple-300 rounded-xl drop-shadow-[0_0_4px_var(--color-primary-purple-200)]">
       {/* Image de l'activité */}
-      <Image 
-        src={`/images/activities/${activity.image_ref}`}
-        alt={`Image de l'activité ${activity.name}`}
-        width={300} 
-        height={200}
-        className="w-full rounded-xl"
-      />
-      {/* Catégorie (badge coloré) */}
-      <span 
-        className="absolute top-3 left-3 rounded-xl text-neutral-50 px-2 md:text-sm text-xs font-medium"
-        style={{ backgroundColor: activity.category.color }}
-      >
-        {activity.category.name}
-      </span>
+      <Link href={`/activities/${activity.id}`}>
+        <Image 
+          src={`/images/activities/${activity.image_ref}`}
+          alt={`Image de l'activité ${activity.name}`}
+          width={300} 
+          height={200}
+          className="w-full rounded-xl"
+        />
+        {/* Catégorie (badge coloré) */}
+        <span 
+          className="absolute top-3 left-3 rounded-xl text-neutral-50 px-2 md:text-sm text-xs font-medium"
+          style={{ backgroundColor: activity.category.color }}
+        >
+          {activity.category.name}
+        </span>
+      </Link>
 
       {/* Contenu texte */}
       <div className="absolute bottom-0 w-full bg-neutral-700/80 flex justify-between items-center px-2 md:py-4 py-2 rounded-xl">
@@ -62,9 +48,9 @@ export default function CardActivity({activity} : PropsCardActivity) {
         </div>
         <Link
           href={`/activities/${activity.id}`}
-          className="self-end hover:text-secondary-300"
+          className="self-end text-neutral-300 hover:text-secondary-300"
         >
-        En savoir plus
+          Explore
         </Link>
       </div>
     </article>
