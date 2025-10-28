566c56b23fd64a381042a8868a3e80067864f72d
melanie-saunier <melanie.sau@wanadoo.fr>
2025-10-28 14:37:46 +0100

    front/activitiesdetails/chore: loading activity details page

--- front-client/src/app/activities/[id]/page.tsx
+++ front-client/src/app/activities/[id]/page.tsx
@@ -3,7 +3,7 @@ import renderSkulls from "@/components/RenderSkulls";
 import { TriangleAlert } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
-import { notFound } from "next/navigation";
+
 
 type ActivityDetailPageProps = {
   params: Promise<{ id: number }>;
