d97ec76a48321e71e7447807a966f4d90cc1aa00
maximilien <to.maximilien@gmail.com>
2025-10-29 12:10:43 +0100

    feat:create mybooking page base structure

---
+++ front-client/src/app/my-booking/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+export default function BookingPage() {
+  return (
+    <section className="min-h-screen p-4 md:p-8">
+      <div className="max-w-4xl mx-auto">
+        <h1 className="text-4xl md:text-5xl font-bold text-center mb-8">
+          Réserver vos billets
+        </h1>
+      </div>
+    </section>
+  );
+}
