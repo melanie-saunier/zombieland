df47d6f6a70326745bc1004af667dca9c69cd5f6
melanie-saunier <melanie.sau@wanadoo.fr>
2025-11-03 21:30:55 +0100

    front/test: test on cardActivity component and skull render

--- front-client/src/__test__/CardActivity.test.tsx
+++ front-client/src/__test__/CardActivity.test.tsx
@@ -15,7 +15,40 @@ const mockActivity = {
 
 describe("CardActivity", () => {
   it("Display the name of the activity", () => {
+    //on met CardAcitvity dans le dom virtuel on ajoute le mock d'activity
     render(<CardActivity activity={mockActivity} />);
+    // on vérifie que le nom de l'activity est présente 
     expect(screen.getByText(/The Grinder/i)).toBeInTheDocument();
   });
+  it("displays the category badge with the correct color", () => {
+    //on met CardAcitvity dans le dom virtuel on ajoute le mock d'activity
+    render(<CardActivity activity={mockActivity} />);
+    // on cherche le badge catégorie
+    const categoryBadge = screen.getByText(/Frissons mécaniques/i);
+    // on teste si il est présent
+    expect(categoryBadge).toBeInTheDocument();
+    //on vérifie que le style est juste
+    expect(categoryBadge).toHaveStyle({ backgroundColor: mockActivity.category.color });
+  });
+
+  it("renders the image with correct alt and src", () => {
+    //on met CardAcitvity dans le dom virtuel on ajoute le mock d'activity
+    render(<CardActivity activity={mockActivity} />);
+    //on cherche l'image de l'activité
+    const img = screen.getByAltText(/Image de l'activité The grinder/i);
+    // on vérifie sa présence
+    expect(img).toBeInTheDocument();
+    // et son lien src
+    expect(img.getAttribute("src")).toContain(mockActivity.image_ref);
+  });
+
+  it("renders the explore link with correct href", () => {
+    //on met CardAcitvity dans le dom virtuel on ajoute le mock d'activity
+    render(<CardActivity activity={mockActivity} />);
+    //on cherche le lien qui contient le texte "explore" dans une balise a
+    const link = screen.getByText(/Explore/i).closest("a");
+    //on vérifie qu'il contient la bonne route
+    expect(link).toHaveAttribute("href", `/activities/${mockActivity.id}`);
+  });
+  
 });
